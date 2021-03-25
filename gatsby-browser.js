19973ca7f7bffc66e6a9df08bd09ede29dfd080a
Ashutosh <ashutosh.tripathi@walmartlabs.com>
2021-03-25 11:03:00 +0530

    changes pages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 /* eslint react/prop-types: 0 */
 import React from 'react'
-import Layout from './src/components/Layout'
+import Layout from './src/components/layout'
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
